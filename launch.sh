c6e935ed5e1f2b6a96cd787a4285f1add50f6470
Empyrean-Void <jshortz@proton.me>
2023-07-18 13:21:51 -0600

    Added bar for laptop screen (iGPU non hybrid)

--- launch.sh
+++ launch.sh
@@ -5,4 +5,8 @@ killall -q polybar
 polybar main 2>&1 | tee -a /tmp/polybar.log & disown
 polybar laptop 2>&1 | tee -a /tmp/polybar.log & disown
 
+if [[ xrandr -q | rg "eDP1 connected" -eq 1 ]]; then
+  polybar iGPU 2>&1 | tee -a /tmp/polybar.log & disown
+fi
+
 echo "polybar launched"
